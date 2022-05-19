c6043005c74778776cdad91eff4c498d8ec6d1c6
Georgios Christodoulou <icsd19245@aegean.gr>
2022-05-20 00:05:36 +0300

    student makes a statement

--- make_statement.php
+++ make_statement.php
@@ -1,15 +1,30 @@
 <?php
 include 'header.php';
 
-echo 'Ολα τα μαθήματα:<br>';
 include('student.php'); 
+include('subject.php'); 
+
 $result = getSubjects4Statement($_SESSION['Username']);
 while($row = mysqli_fetch_array($result, MYSQLI_ASSOC))
-{
+{    
     $name = $row['subname'];
-    
-    echo $name."<br>";
+    $subject_id = get_subject_id($name);
+    if (isset($_POST[$subject_id])){
+        make_statement($_SESSION['Username'], $name);
+    }
 }
+
+echo '<br>Ολα τα μαθήματα:<br>';
+$result = getSubjects4Statement($_SESSION['Username']);
+echo "<BR><form action='make_statement.php' method='post'>";
+while($row = mysqli_fetch_array($result, MYSQLI_ASSOC))
+{    
+    $name = $row['subname'];
+    $subject_id = get_subject_id($name);
+    echo "<input type='checkbox' name='".$subject_id."'>".$name."<br>";
+}
+echo "<button type='submit'>Δήλωση</button>";
+echo "</form>"
 ?>
 </body>
 </div>
--- student.php
+++ student.php
@@ -3,7 +3,7 @@
 
         include('connection.php'); 
 
-        $sql = "SELECT sub.name as subname 
+        $sql = "SELECT sub.name as subname, sta.final_grade as final_grade, lec.year as yearr 
         FROM statement sta, user u, lecture lec, subject sub 
         WHERE u.registration_num_id = sta.student_id and u.login = ? and lec.id = sta.lecture_id and sub.id = lec.subject_id";
 
@@ -40,7 +40,11 @@
         and sub.id not in (
             SELECT lec4.subject_id
             FROM statement sta4, lecture lec4
-            WHERE u.login = ? and u.registration_num_id = sta4.student_id and lec4.id = sta4.lecture_id and sta4.final_grade is NULL)";
+            WHERE u.login = ? and u.registration_num_id = sta4.student_id and lec4.id = sta4.lecture_id and sta4.final_grade is NULL)
+        and sub.id IN (
+            SELECT lec5.subject_id
+            from lecture lec5
+            where lec5.year = 2022 and mod(lec5.semester, 2) = 0)";
 
         $stmnt = mysqli_prepare($con, $sql);
         $stmnt->bind_param("sssss",$username, $username, $username, $username, $username);
@@ -48,6 +52,41 @@
 
         $result = $stmnt->get_result();  
         return $result;
+    }
+
+    function get_student_id($username) {
+        include('connection.php'); 
+
+        $sql = "SELECT stu.registration_num as id
+        FROM user u, student stu 
+        where u.registration_num_id = stu.registration_num and u.login = ?";
+
+        $stmnt = mysqli_prepare($con, $sql);
+        $stmnt->bind_param("s",$username);
+        $stmnt->execute();
+
+        $result = $stmnt->get_result();  
+        while($row = mysqli_fetch_array($result, MYSQLI_ASSOC))
+        {    
+            $id = $row['id'];
+            return $id;
+        }
+        return NULL;
+    }
+
+    function make_statement($username, $subject_name) {
+        include('connection.php'); 
+        
+        $student_id = get_student_id($username);        
+        $lecture_id = get_lecture_id($subject_name);
+        
+        $sql = "INSERT INTO statement (lecture_id, student_id) VALUES ($lecture_id, $student_id)";
+        if ($con->query($sql) === TRUE) {
+            echo "Το μάθημα " . $subject_name . " δηλώθηκε επιτυχώς.<BR>";
+        } else {
+            echo "Πρόβλημα κατά τη δήλωση του μαθήματος " . $subject_name;
+        }
+
     }
     
 ?>
