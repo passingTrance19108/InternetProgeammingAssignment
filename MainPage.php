c18c7c422ba110a268400b462a034bc9d6e28a70
Konstantinos Lales <icsd19108@aegean.gr>
2022-05-28 19:40:39 +0300

    Beutified Teacher course to grade selection(Main)

--- MainPage.php
+++ MainPage.php
@@ -1,8 +1,9 @@
 <?php
 include 'Components/header.php';
 ?>
-
+    
         <?php
+        echo "<div class='container'>";
         if ($_SESSION['Role'] == 'student') {
             echo 'Ολα τα μαθήματα:<br>';
             include('student.php'); 
@@ -18,23 +19,36 @@ include 'Components/header.php';
             echo "<a href='make_statement.php'>Δήλωση μαθήματος</a>";
         }
         if ($_SESSION['Role'] == 'Teacher') {
-            echo 'Επιλέξτε μάθημα για βαθμολόγηση:<br>';
+            echo "<p><h6 class='text-center'>Επιλέξτε μάθημα για βαθμολόγηση:</h6></p><br>";
             include('teacher.php'); 
             $result = getAllSubjects($_SESSION['Username']);
+            echo "<table class='table table-bordered'>";
             while($row = mysqli_fetch_array($result, MYSQLI_ASSOC))
             {
                 $lecid = $row['lecid'];
                 $name = $row['subname'];
                 $semester = $row['semester'];
                 $year = $row['yearr'];
+                ?>
                 
-                echo "<a href='grade.php?lecid=" . $lecid . "&name=" . $name . "'>".$name." - " . $year . " - " . $semester . "</a><br>";
+                <tr>
+                
+                    <td><a href="grade.php?lecid=<?php echo $lecid . "&name=" . $name ?>"><?php echo $name; ?></a></td>
+                    <td><?php echo $year; ?></td>
+                    <td><?php echo $semester; ?></td>
+                
+                </tr>
+                
+                <?php
+                // echo "<a href='grade.php?lecid=" . $lecid . "&name=" . $name . "'>".$name." - " . $year . " - " . $semester . "</a><br>";
             }
+            echo "</table>";
             
         }
         if ($_SESSION['Role'] == 'administrator') {
             echo "<a href='insertSubject.php'>Εισαγωγή μαθήματος</a>";
         }
+        echo "</div>";
         ?>
 
 </div>
